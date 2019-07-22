afc841d8b33146ed25fdf66e9898da0a4f8ed3a0
kubota <sakaku193@gmail.com>
2019-07-22 17:20:48 +0900

    リファクタリング前

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
-常識の範囲内で使ってください。
-
+・必要最低限
 npm i ytdl-core
 https://www.npmjs.com/package/ytdl-core
+
+・高画質対応用
+npm install --save fluent-ffmpeg
+https://github.com/fluent-ffmpeg/node-fluent-ffmpeg
+npm i @ffmpeg-installer/ffmpeg
+
+
+※最高画質でダウンロードしたい場合の手順
--- main.js
+++ main.js
@@ -1,18 +1,60 @@
-const fs = require('fs');
+const fs = require('fs')
 
-const ytdl = require('ytdl-core');
-const readline = require('readline');
+const ytdl = require('ytdl-core')
+const readline = require('readline')
+const ffmpegPath = require('@ffmpeg-installer/ffmpeg').path
+const ffmpeg = require('fluent-ffmpeg')
+ffmpeg.setFfmpegPath(ffmpegPath)
 
 let rl = readline.createInterface({
   input: process.stdin
 });
+let mode = false
 
 rl.on('line', line => {
   if(line == undefined || line == ''){
-    return;
+    return
   }
-  ytdl.getInfo(line, function(err, info) {
-    console.log(info.title.replace(/ /g, ''));
-    ytdl(line).pipe(fs.createWriteStream('download/' + info.title.replace(/\//g, '／') + '.mp4'));
-  });
+  if (line == 'mode' && mode == true) {
+    mode = false
+    console.log("off")
+    return
+  }else if(line == 'mode' && mode == false){
+    mode = true
+    console.log("on")
+    return
+  }
+
+  if(mode){
+    const audioOutput = 'download/sound.mp4';
+    const mainOutput = 'download/output.mp4';
+    ytdl(line, { filter: format => {
+      return format.container === 'm4a' && !format.encoding; } })
+      .pipe(fs.createWriteStream(audioOutput))
+      .on('finish', () => {
+        ffmpeg()
+          .input(ytdl(line, { filter: format => {
+            return format.container === 'mp4' && !format.audioEncoding; } }))
+          .videoCodec('copy')
+          .input(audioOutput)
+          .audioCodec('copy')
+          .save(mainOutput)
+          .on('error', console.error)
+          .on('progress', progress => {
+            process.stdout.cursorTo(0);
+            process.stdout.clearLine(1);
+            process.stdout.write(progress.timemark);
+          }).on('end', () => {
+            fs.unlink(audioOutput, err => {
+              if(err) console.error(err);
+              else console.log('\nfinished downloading');
+            });
+          });
+      });
+  }else{
+    ytdl.getInfo(line, function(err, info) {
+      console.log(info.title.replace(/ /g, ''))
+      ytdl(line).pipe(fs.createWriteStream('download/' + info.title.replace(/\//g, '／') + '.mp4'))
+    });
+  }
 });
