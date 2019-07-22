d49e28c8d7e21261ce8ffeb128c3ed4c2cf8779e
kubota <sakaku193@gmail.com>
2019-07-22 15:21:08 +0900

    npm install --save fluent-ffmpeg

--- main.js
+++ main.js
@@ -3,18 +3,16 @@ const fs = require('fs');
 const ytdl = require('ytdl-core');
 const readline = require('readline');
 
-var rl = readline.createInterface({
+let rl = readline.createInterface({
   input: process.stdin
 });
 
 rl.on('line', line => {
-  if(line == null || line == ''){
+  if(line == undefined || line == ''){
     return;
   }
-
   ytdl.getInfo(line, function(err, info) {
     console.log(info.title.replace(/ /g, ''));
     ytdl(line).pipe(fs.createWriteStream('download/' + info.title.replace(/\//g, '／') + '.mp4'));
   });
-
 });
