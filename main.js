b1a6786b19be196d67341d39ec2ecba8313efc5a
kubota <sakaku193@gmail.com>
2019-07-22 11:52:21 +0900

    よく考えたら不要な部分を削除

--- main.js
+++ main.js
@@ -3,8 +3,6 @@ const fs = require('fs');
 const ytdl = require('ytdl-core');
 const readline = require('readline');
 
-const BASE_PATH = 'https://www.youtube.com/watch?v=';
-
 var rl = readline.createInterface({
   input: process.stdin
 });
@@ -13,21 +11,10 @@ rl.on('line', line => {
   if(line == null || line == ''){
     return;
   }
-  var youtube_id = getParam('v', line);
 
-  ytdl.getInfo(BASE_PATH + youtube_id, function(err, info) {
-    var title = info.title;
+  ytdl.getInfo(line, function(err, info) {
     console.log(info.title.replace(/ /g, ''));
-    ytdl(BASE_PATH + youtube_id).pipe(fs.createWriteStream('download/' + info.title.replace(/\//g, '／') + '.mp4'));
+    ytdl(line).pipe(fs.createWriteStream('download/' + info.title.replace(/\//g, '／') + '.mp4'));
   });
 
 });
-
-function getParam(name, url){
-  name = name.replace(/[\[\]]/g, "\\$&");
-  var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
-      results = regex.exec(url);
-  if (!results) return null;
-  if (!results[2]) return '';
-  return decodeURIComponent(results[2].replace(/\+/g, " "));
-}
