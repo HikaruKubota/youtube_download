1cbd27c9ade726196d1557db5e1d6afafa0466ac
sakaku193 <sakaku193@gmail.com>
2021-01-16 08:46:11 +0900

    filenameをuuidに変更した

--- README.md
+++ README.md
@@ -1,4 +1,1 @@
 常識の範囲内で使ってください。
-
-npm i ytdl-core
-https://www.npmjs.com/package/ytdl-core
--- main.js
+++ main.js
@@ -2,6 +2,14 @@ const fs = require('fs');
 
 const ytdl = require('ytdl-core');
 const readline = require('readline');
+const uuid = require('uuid');
+
+var u = uuid.v1({
+    node: [0X01, 0X02, 0X03, 0X04, 0X05, 0X06],
+    clockseq: 0X1234,
+    msecs: new Date(),
+    nsecs: 1234
+}, buf, 0);
 
 let rl = readline.createInterface({
   input: process.stdin
@@ -12,7 +20,8 @@ rl.on('line', line => {
     return;
   }
   ytdl.getInfo(line, function(err, info) {
-    console.log(info.title.replace(/\//g, ''));
-    ytdl(line).pipe(fs.createWriteStream('download/' + info.title.replace(/\//g, '／') + '.mp4'));
+    // console.log(info.title.replace(/\//g, ''));
+    // ytdl(line).pipe(fs.createWriteStream('download/' + info.title.replace(/\//g, '／') + '.mp4'));
+    ytdl(line).pipe(fs.createWriteStream('download/' + u + '.mp4'));
   });
 });
